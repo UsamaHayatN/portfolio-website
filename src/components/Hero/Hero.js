361fff2b920467f825a589e36899afefaef6b819
UsamaHayatN <usamahayatnissoana@gmail.com>
2023-02-23 01:51:26 +0800

    initial commit

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        I'm proficient in Front-end React JS frameworks. My passion lies in learning about the latest
+        technologies. Solution-oriented and problem solver with experience building and maintaining
+        software and software architecture. Highly skilled in communication, collaboration, and technical
+        documentation.
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
+);
+
+export default Hero;
